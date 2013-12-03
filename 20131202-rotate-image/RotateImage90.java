bebffbc96bea6e3e5e5fc150c025a12de1a22f37
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2013-12-02 23:13:44 -0600

    Add quick test. Looks right.

--- 20131202-rotate-image/RotateImage90.java
+++ 20131202-rotate-image/RotateImage90.java
@@ -6,8 +6,25 @@ public class RotateImage90 {
 
         for (int i = 0; i < N; i++)
             for (int j = 0; j < N; j++)
-                ar[N - j - 1][i] = a[i][j];
+                ar[N-j-1][i] = a[i][j];
 
         return ar;
+    }
+
+    private static void print(int[][] a) {
+        int N = a.length;
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++) {
+                System.out.printf("%d ", a[i][j]);
+                if (j == N-1) System.out.printf("\n");
+            }
+    }
+
+    public static void main(String[] args) {
+        int[][] a = {{0,1,2},{3,4,5},{6,7,8}};
+        int[][] b = rotate90(a);
+        print(a);
+        System.out.println();
+        print(b);
     }
 }
