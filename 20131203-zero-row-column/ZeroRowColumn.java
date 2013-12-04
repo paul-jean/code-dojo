65912fd5523f768632375fc1dd29eb1495bda0d2
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2013-12-03 22:23:32 -0600

    Add a test.

--- 20131203-zero-row-column/ZeroRowColumn.java
+++ 20131203-zero-row-column/ZeroRowColumn.java
@@ -5,7 +5,8 @@ public class ZeroRowColumn {
 
         int N = a[0].length;
         int M = a.length;
-        int[][] az = Arrays.copyOf(a);
+        int[][] az = new int[M][N];
+        for (int i = 0; i < M; i++) az[i] = Arrays.copyOf(a[i], N);
         int[] zero_row = new int[N];
         for (int j = 0; j < N; j++) zero_row[j] = 0;
 
@@ -18,5 +19,23 @@ public class ZeroRowColumn {
             }
 
         return az;
+    }
+
+    private static void printArray(int[][] a) {
+        for (int i = 0; i < a.length; i++)
+            for (int j = 0; j < a[0].length; j++) {
+                System.out.printf("%-2d", a[i][j]);
+                if (j == a[0].length - 1) System.out.printf("\n");
+                else System.out.printf(" ");
+            }
+    }
+
+    public static void main(String args[]) {
+        int[][] test = {{1, 2, 3}, {4, 0, 6}, {7, 8, 9}, {10, 11, 12}};
+        System.out.println("array:");
+        printArray(test);
+        int[][] testz = setZeroRowColumn(test);
+        System.out.println("zero'd:");
+        printArray(testz);
     }
 }
