0dca0af7bad80a9a6e68129733ac23d1256901d3
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2013-12-04 23:11:12 -0600

    Had args mixed up for isSubstring.

--- 20131204-is-rotation/TestRotation.java
+++ 20131204-is-rotation/TestRotation.java
@@ -5,7 +5,7 @@ public class TestRotation {
         int n1 = s1.length();
         String first_half = s1.substring(0, n1/2);
         String last_half = s1.substring(n1/2 + 1, n1 - 1);
-        if (isSubstring(first_half, s2) || isSubstring(last_half, s2))
+        if (isSubstring(s2, first_half) || isSubstring(s2, last_half))
             return true;
         return false;
     }
