c8c51a435d1fde09f8ed89a5539fae2311e324cd
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2013-12-11 21:44:01 -0600

    Delete a given node from a LL (before compiling).

---
+++ 20131211-delete-ll-node/DeleteLLNode.java
@@ -0,0 +1,24 @@
+public class DeleteLLNode {
+
+    public void deleteNode(Node n) {
+        if (n == null) throw new NullPointerException();
+
+        n = n.getNext();
+    }
+
+    public static void main(String[] args) {
+
+        Node head = new Node(1);
+        head.appendToTail(2);
+        head.appendToTail(3);
+        head.appendToTail(4);
+        head.appendToTail(5);
+
+        System.out.println("LL:");
+        head.print();
+        Node n = head.getNext().getNext();
+        deleteNode(n);
+        System.out.println("After deleting node 3:");
+        head.print();
+    }
+}
