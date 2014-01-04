ab1fcf17802aaf7e8d8c8393a6e645f37ad04823
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2014-01-03 21:59:26 -0600

    Fix compile-time errors.  Still not creating the proper lists though.

--- 20130103-tree-to-lists/TreeNode.java
+++ 20130103-tree-to-lists/TreeNode.java
@@ -9,4 +9,37 @@ public class TreeNode {
         left = null;
         right = null;
     }
+
+    @Override
+    public String toString() {
+        int depth = maxDepth(this);
+        int charsPerLine = (int) Math.pow(2, depth);
+        char[][] a = new char[depth][charsPerLine];
+        for (int i = 0; i < depth; i++)
+            for (int j = 0; j < charsPerLine; j++) {
+                if (j == charsPerLine - 1) a[i][j] = '\n';
+                else a[i][j] = ' ';
+            }
+        int x = (int) Math.pow(2, depth - 1);
+        printNode(this, 0, x, x, a);
+        String s = "";
+        for (int i = 0; i < depth; i++)
+            for (int j = 0; j < charsPerLine; j++) s += a[i][j];
+        return s;
+    }
+
+    private void printNode(TreeNode n, int level, int x, int d, char[][] a) {
+        if (n == null) return;
+        int node = n.data;
+        a[level][x] = Character.forDigit(node, 10);
+        int xLeft = x - d/2;
+        int xRight = x + d/2;
+        printNode(n.left, level + 1, xLeft, d/2, a);
+        printNode(n.right, level + 1, xRight, d/2, a);
+    }
+
+    private static int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
 }
--- 20130103-tree-to-lists/TreeToLists.java
+++ 20130103-tree-to-lists/TreeToLists.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class TreeToLists {
 
     public static Node[] heads;
@@ -13,29 +15,35 @@ public class TreeToLists {
 
         if (root == null) return;
         if (heads.length < level) {
-            Node[] headsNew = new Node[heads.length + 1];
-            Arrays.copyOf(heads, headsNew, 0, heads.length - 1);
+            Node[] headsNew = Arrays.copyOf(heads, heads.length + 1);
             heads = headsNew;
         }
         if (heads[level - 1] == null) heads[level - 1] = new Node(root.data);
-        else heads[level - 1].appendToList(root.data);
+        else heads[level - 1].appendToTail(root.data);
         levelLists(root.left, level + 1, heads);
         levelLists(root.right, level + 1, heads);
     }
 
     public static void main(String[] args) {
+
         TreeNode root = new TreeNode(1);
         root.left = new TreeNode(2);
         root.right = new TreeNode(3);
         root.left.left = new TreeNode(4);
+        String treeString = root.toString();
+        System.out.println("Tree:");
+        System.out.printf("%s", treeString);
+        System.out.println();
+        System.out.println("Lists:");
         Node[] heads = treeToLists(root);
+        System.out.println("heads.length = " + heads.length);
         Node h = null;
         int level = 1;
         for (Node head: heads) {
             h = head;
-            System.out.printf("level %d:", level);
+            System.out.printf("level %d: ", level);
             while (h != null) {
-                System.out.printf("%d ->", h.data);
+                System.out.printf(" %d ->", h.data);
                 h = h.next;
             }
             level++;
