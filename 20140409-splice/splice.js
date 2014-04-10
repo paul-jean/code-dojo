ed56bf01b93f73e5990ba3aa8c5b270ff317a36d
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2014-04-09 23:00:28 -0400

    Remove debug prints.

--- 20140409-splice/splice.js
+++ 20140409-splice/splice.js
@@ -6,19 +6,11 @@ var splice = function (array, start, numDelete) {
   var leftChunkStop = start - 1;
   var rightChunkStart = start + numDelete;
   var rightChunkStop = array.length;
-  console.log('leftChunkStart = ' + leftChunkStart);
-  console.log('leftChunkStop = ' + leftChunkStop);
-  console.log(array.slice(leftChunkStart, leftChunkStop + 1));
-  console.log('rightChunkStart = ' + rightChunkStart);
-  console.log('rightChunkStop = ' + rightChunkStop);
-  console.log(array.slice(rightChunkStart, rightChunkStop + 1));
   if (numDelete < 0) throw {name: 'BadDelete', message: 'Cannot delete ' + numDelete + ' elements'};
   var deleted = array.slice(start, start + numDelete);
   var inserted = [];
   for (var i = 3; i < arguments.length; i++)
     inserted.push(arguments[i]);
-  console.log('inserted = ');
-  console.log(inserted);
   b = b.concat(array.slice(leftChunkStart, leftChunkStop + 1))
       .concat(inserted)
       .concat(array.slice(rightChunkStart, rightChunkStop + 1));
