400a8c9d5f110f846b3cb1200e43d7e3fb8a8e64
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2014-05-26 21:07:42 -0500

    Iterative implementation passing basic tests.

--- 20140526-fib-iterative/fib.js
+++ 20140526-fib-iterative/fib.js
@@ -1,11 +1,17 @@
 var fib = function(n) {
-  var fs = [0, 1, 2],
-      fNew;
-  if (n === 1) return 0;
-  if (n === 2) return 1;
+  var fs = [0, 1], fNew;
+  if (n < 3) {
+    return fs[n - 1];
+  }
   for (var i = 0; i < n - 2; i ++) {
-    fNew = fs[1] + fs[2];
-    fs = [fs[1], fs[2], fNew];
+    fNew = fs[0] + fs[1];
+    fs.push(fNew);
+    fs.shift();
   }
   return fs[fs.length - 1];
+};
+
+var util = require('util');
+for (var n = 1; n <= 10; n ++) {
+  console.log(util.format("%d\t%d", n, fib(n)));
 }
