b6530db74199ff0a24fb168eced534803b05946e
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2014-05-27 23:03:43 -0500

    Compare number of fib calls in memoized and non-memoized versions.

--- 20140526-fib-memoized/fib.js
+++ 20140526-fib-memoized/fib.js
@@ -1,30 +1,49 @@
 var closureMemo = function (val) {
-  var numCalls = 0;
   return {
-    getValue: function() {numCalls++; return val;},
-    getCalls: function() {
-      var indent = "";
-      for (var i = 0; i < numCalls; i ++) {
-        indent += "-";
-      }
-      return indent;
-    }
+    getValue: function() {return val;}
   };
 };
+
+var numCalls = {};
 
 var fibMemo = {
   1: closureMemo(0),
   2: closureMemo(1)
 };
 
-var fib = function(n) {
+var indent = function(n) {
+  var dashes = "";
+  for (var i = 0; i < n; i ++) {
+    dashes += "-";
+  }
+  return dashes;
+};
+
+var fibMem = function(n) {
+  if (typeof numCalls[n] === 'number') {
+    numCalls[n] += 1;
+  } else {
+    numCalls[n] = 0;
+  }
+  console.log(indent(numCalls[n]) + " " + n);
   if (typeof fibMemo[n] === 'object') {
-    console.log(fibMemo[n].getCalls() + " " + n);
     return fibMemo[n].getValue();
   } else {
-    fibMemo[n] = closureMemo(fib(n - 2) + fib(n - 1));
+    fibMemo[n] = closureMemo(fibMem(n - 2) + fibMem(n - 1));
     return fibMemo[n].getValue();
   }
+};
+
+var fib = function(n) {
+  if (typeof numCalls[n] === 'number') {
+    numCalls[n] += 1;
+  } else {
+    numCalls[n] = 0;
+  }
+  console.log(indent(numCalls[n]) + " " + n);
+  if (n === 1) { return 0; }
+  if (n === 2) { return 1; }
+  return fib(n - 2) + fib(n - 1);
 };
 
 // console.log(fib(1));
@@ -32,7 +51,17 @@ var fib = function(n) {
 // console.log(typeof fibMemo[1]);
 
 var util = require('util');
+console.log("Memoized fib ...");
+for (var n = 1; n <= 10; n ++) {
+  console.log(util.format("%d\t%d", n, fibMem(n)));
+}
+console.log(numCalls);
+
+numCalls = {};
+
+console.log("Regular fib ...");
 for (var n = 1; n <= 10; n ++) {
   console.log(util.format("%d\t%d", n, fib(n)));
 }
-// console.log(fibMemo);
+console.log(numCalls);
+
