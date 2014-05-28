ebecd086d94b7b9fc93e57c4897b7d2431546b19
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2014-05-27 22:09:30 -0500

    Create a closure around the memoized values.

--- 20140526-fib-memoized/fib.js
+++ 20140526-fib-memoized/fib.js
@@ -1,25 +1,27 @@
-var fibMemo = {1:0, 2:1};
-var fibCalls = {};
+var fibMemo = {
+  1: {getValue: function() {return 0;}},
+  2: {getValue: function() {return 1;}}
+};
+
 var fib = function(n) {
-  if (!fibCalls[n]) {
-    fibCalls[n] = 1;
+  if (typeof fibMemo[n] === 'object') {
+    return fibMemo[n].getValue();
   } else {
-    fibCalls[n] += 1;
-  }
-  if (typeof fibMemo[n] === 'number') {
-    //    console.log("memo " + n);
-    return fibMemo[n];
-  } else {
-    fibMemo[n] = fib(n - 2) + fib(n - 1);
-    return fibMemo[n];
+    fibMemo[n] = function(val) {
+      return {
+        getValue: function() { return val; }
+      };
+    }(fib(n - 2) + fib(n - 1));
+    return fibMemo[n].getValue();
   }
 };
 
-//console.log(fib(1));
+console.log(fib(1));
+// console.log(fibMemo[1].getValue());
+// console.log(typeof fibMemo[1]);
 
 var util = require('util');
-//console.log(util.format("%d\t%d", 1, fib(1)));
 for (var n = 1; n <= 10; n ++) {
   console.log(util.format("%d\t%d", n, fib(n)));
 }
-console.log(fibCalls);
+console.log(fibMemo);
