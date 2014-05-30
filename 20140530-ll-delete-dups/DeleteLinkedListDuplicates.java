82fee572e7ca8c3e518653bb261db07c34195ca5
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2014-05-30 12:30:42 -0500

    Fixes NPE but needs a cleaner implementation.

--- 20140530-ll-delete-dups/DeleteLinkedListDuplicates.java
+++ 20140530-ll-delete-dups/DeleteLinkedListDuplicates.java
@@ -15,17 +15,24 @@ public class DeleteLinkedListDuplicates {
     Hashtable seen = new Hashtable();
     Node current = head;
     seen.put(current.data, true);
-    while (current.next != null) {
-      System.out.println("Checking " + current.next.data);
-      if (seen.containsKey(current.next.data)) {
+    while (current != null) {
+      //System.out.println("Checking: " + current.next.data);
+      if (current.next != null && seen.containsKey(current.next.data)) {
+        System.out.println("Found duplicate: " + current.next.data);
         current.next = current.next.next;
       }
-      seen.put(current.next.data, true);
+      if (current.next != null) {
+        //System.out.println("Storing:");
+        //System.out.println(current.next.data);
+        seen.put(current.next.data, true);
+      }
+      //System.out.println("current -> current.next");
       current = current.next;
+      //System.out.println("current set");
     }
   }
 
-  public void run() {
+  public void test1() {
     Node head = new Node(0);
     Node current = head;
     current.next = new Node(1);
@@ -33,9 +40,43 @@ public class DeleteLinkedListDuplicates {
     current.next = new Node(2);
     current = current.next;
     current.next = new Node(1);
-    deleteDuplicates(head);
+    System.out.println("List elements:");
     current = head;
-    while (current.next != null) {
+    while (current != null) {
+      System.out.println(current.data);
+      current = current.next;
+    }
+    System.out.println("Deleting duplicates ...");
+    deleteDuplicates(head);
+    System.out.println("New list:");
+    current = head;
+    while (current != null) {
+      System.out.println(current.data);
+      current = current.next;
+    }
+  }
+
+  public void test2() {
+    Node head = new Node(0);
+    Node current = head;
+    current.next = new Node(1);
+    current = current.next;
+    current.next = new Node(2);
+    current = current.next;
+    current.next = new Node(1);
+    current = current.next;
+    current.next = new Node(4);
+    System.out.println("List elements:");
+    current = head;
+    while (current != null) {
+      System.out.println(current.data);
+      current = current.next;
+    }
+    System.out.println("Deleting duplicates ...");
+    deleteDuplicates(head);
+    System.out.println("New list:");
+    current = head;
+    while (current != null) {
       System.out.println(current.data);
       current = current.next;
     }
@@ -43,6 +84,7 @@ public class DeleteLinkedListDuplicates {
 
   public static void main(String[] args) {
     DeleteLinkedListDuplicates dlld = new DeleteLinkedListDuplicates();
-    dlld.run();
+    dlld.test1();
+    dlld.test2();
   }
 }
