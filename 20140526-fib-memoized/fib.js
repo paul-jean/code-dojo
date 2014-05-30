063168b2d7dbc9daf8a5f18593eb083bbec0c588
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2014-05-30 12:56:16 -0500

    Compare calls to fib(10)

--- 20140526-fib-memoized/fib.js
+++ 20140526-fib-memoized/fib.js
@@ -51,17 +51,23 @@ var fib = function(n) {
 // console.log(typeof fibMemo[1]);
 
 var util = require('util');
-console.log("Memoized fib ...");
-for (var n = 1; n <= 10; n ++) {
-  console.log(util.format("%d\t%d", n, fib_mem(n)));
-}
+console.log("Evaluating memoized fib ...");
+console.log("fib_mem(10)");
+// for (var n = 1; n <= 10; n ++) {
+//   console.log(util.format("%d\t%d", n, fib_mem(n)));
+// }
+fib_mem(10);
+console.log("n -> # of calls to fib_mem(n)");
 console.log(numCalls);
 
 numCalls = {};
 
-console.log("Regular fib ...");
-for (var n = 1; n <= 10; n ++) {
-  console.log(util.format("%d\t%d", n, fib(n)));
-}
+console.log("\nEvaluating regular fib ...");
+console.log("fib(10)");
+// for (var n = 1; n <= 10; n ++) {
+//   console.log(util.format("%d\t%d", n, fib(n)));
+// }
+fib(10);
+console.log("n -> # of calls to fib(n)");
 console.log(numCalls);
 
