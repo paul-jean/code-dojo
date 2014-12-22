13c5cd81e9b7065030ef5b02aca2d65242735545
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2014-12-21 18:36:17 -0700

    added hover functions to hover.js that turn text red and make an alert

--- 20141220-html-helloworld/hover.js
+++ 20141220-html-helloworld/hover.js
@@ -1,3 +1,23 @@
-var f = function (foo) {
-    return foo;
+var hover = document.getElementsByClassName("hover");
+
+var myObj = {
+    myField: 1,
+    myMethod: function () {
+        window.alert("myMethod being executed.");
+    },
+    anotherField: {
+        nestedField: 2
+    }
 };
+
+
+var color = function(){
+    hover[0].style.color = "red";
+    console.log(myObj.myField);
+    console.log(myObj.myMethod());
+    console.log(myObj.anotherField.nestedField);
+};
+
+hover[0].addEventListener("mouseover", color);
+
+
