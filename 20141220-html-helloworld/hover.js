34861febb39ee52519a13836b0bdbee5e2527209
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2014-12-23 15:43:07 -0700

    added code snippet to js file that turns all text purple and commented out previous on hover code. Changed class to ID in H1 of index.html

--- 20141220-html-helloworld/hover.js
+++ 20141220-html-helloworld/hover.js
@@ -1,4 +1,5 @@
-var hover = document.getElementsByClassName("hover");
+
+/*var hover = document.getElementsByClassName("hover");
 
 var myObj = {
     myField: 1,
@@ -11,13 +12,20 @@ var myObj = {
 };
 
 
-var color = function(){
+var ChangeColor = function(){
     hover[0].style.color = "red";
     console.log(myObj.myField);
     console.log(myObj.myMethod());
     console.log(myObj.anotherField.nestedField);
 };
 
-hover[0].addEventListener("mouseover", color);
+hover[0].addEventListener("mouseover", ChangeColor);*/
+
+  document.getElementById('hover').onclick = changeColor;   
+
+    function changeColor() {
+        document.body.style.color = "purple";
+        return false;
+    }
 
 
