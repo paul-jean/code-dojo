5c71310dd9cd9857e2db0dc545786356fdd82d5c
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2015-09-22 22:29:37 -0400

    Fix test harness. Add jsdoc strings.

--- 20150922-fib-js/fib.js
+++ 20150922-fib-js/fib.js
@@ -1,7 +1,7 @@
 /**
  * Compute the nth fibonacci number.
- * @param n: integer n=0,1,2,... to compute f(n)
- * @return the nth fibonacci number fib(n)
+ * @param {Number} n - the index of fibonacci number to compute
+ * @returns {Number | null} - the nth fibonacci number, or null if n < 0
  *
  */
 var fib = function(n) {
@@ -11,20 +11,25 @@ var fib = function(n) {
     return fib(n-1) + fib(n-2);
 }
 
-// tests
-//
-answers = {
-    0: 1,
-    1: 1,
-    2: 2,
-    3: 3,
-    4: 5,
-    20: 10946,
-    40: 165580141,
-    '-1': null
-};
-
+/**
+ * Test the given fibonacci function.
+ * Tests if fib_fn(n) is correct for several values of n, and prints the result
+ * of each test. Prints a success/fail message when all tests are completed to
+ * indicate if all tests passed (PASSED) or any tests failed (FAILED).
+ * @param {Object} fib_fn - fibonacci function to test.
+ * @returns {null}
+ */
 var test_fib = function (fib_fn) {
+    var answers = {
+        0: 1,
+        1: 1,
+        2: 2,
+        3: 3,
+        4: 5,
+        20: 10946,
+        40: 165580141,
+        '-1': null
+    };
     var success = true;
     for (var n in answers) {
         if (!answers.hasOwnProperty(n)) continue;
@@ -44,6 +49,13 @@ console.log('test: fib');
 test_fib(fib);
 
 var mem = {0:1, 1:1};
+/**
+ * Compute the nth fibonacci number, using memoization to prevent stack
+ * overflow.
+ * @param {Number} n - the index of fibonacci number to compute
+ * @returns {Number | null} - the nth fibonacci number, or null if n < 0
+ *
+ */
 var fib_mem = function(n) {
     if (n < 0) return null;
     if (!mem[n]) {
@@ -52,5 +64,6 @@ var fib_mem = function(n) {
     return mem[n];
 }
 
+console.log();
 console.log('test: fib_mem');
 test_fib(fib_mem);
