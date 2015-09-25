7747e748591d8b1b4d58f6c52473b3a135df5110
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2015-09-25 00:11:12 -0400

    Add fib and fib_mem headings.

--- 20150922-fib-js/print-fib.js
+++ 20150922-fib-js/print-fib.js
@@ -2,19 +2,42 @@ fib = require('./fib.js');
 $ = require('jquery');
 
 var intervalID;
+var n = 0;
+var max_time_diff = 0, max_time_diff_mem = 0;
+var max_time_allowed = 3000; // 3 seconds
 
 var execute = function() {
     var result_div = $('#result');
-    var n = 0;
     var run_nth = function() {
         var val, output = '', time_before, time_after, time_diff;
-        time_before = new Date();
-        val = fib.fib(n);
-        time_after = new Date();
-        time_diff = time_after - time_before;
-        output += '<p> n = ' + n + ',  f(' + n + ') = ' + val + ' (' + time_diff + ' ms) </p>';
-        result_div.append('<div class="nth_result">' + output + '</div>');
-        output = '';
+        // compute fib(n) using straight recursion
+        if (max_time_diff < max_time_allowed) {
+            output = '';
+            time_before = new Date();
+            val = fib.fib(n);
+            time_after = new Date();
+            time_diff = time_after - time_before;
+            max_time_diff = Math.max(time_diff, max_time_diff);
+            output += 'n = ' + n + ',  f(' + n + ') = ' + val + ' (' + time_diff + ' ms)';
+            result_div.append('<div class="nth_result">' + output + '</div>');
+        } else {
+            result_div.append('<div class="nth_result timeout">' + 'TIMEOUT' + '</div>');
+        }
+
+        // compute fib(n) using recursion with memoization:
+        if (max_time_diff_mem < max_time_allowed) {
+            output = '';
+            time_before = new Date();
+            val = fib.fib_mem(n);
+            time_after = new Date();
+            time_diff = time_after - time_before;
+            max_time_diff_mem = Math.max(time_diff, max_time_diff_mem);
+            output += 'n = ' + n + ',  f(' + n + ') = ' + val + ' (' + time_diff + ' ms)';
+            result_div.append('<div class="nth_result_mem">' + output + '</div>');
+        } else {
+            result_div.append('<div class="nth_result_mem timeout">' + 'TIMEOUT' + '</div>');
+        }
+
         n++;
     };
     intervalID = setInterval(run_nth, 500);
