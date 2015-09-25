f9f23da41896b50ce3beb4b34f5962e96d0bed55
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2015-09-24 23:39:50 -0400

    Add iterations to page async.

--- 20150922-fib-js/dist/bundle.js
+++ 20150922-fib-js/dist/bundle.js
@@ -9245,23 +9245,35 @@ return jQuery;
 fib = require('./fib.js');
 $ = require('jquery');
 
+var intervalID;
+
 var execute = function() {
     var result_div = $('#result');
-    var val, output = '', n, time_before, time_after, time_diff;
-    for (n = 0; n < 40; n++) {
+    var n = 0;
+    var run_nth = function() {
+        var val, output = '', time_before, time_after, time_diff;
         time_before = new Date();
         val = fib.fib(n);
         time_after = new Date();
         time_diff = time_after - time_before;
-        output += '<p> n = ' + n + ',  f(n) = ' + val + ' (' + time_diff + ' ms) </p>';
+        output += '<p> n = ' + n + ',  f(' + n + ') = ' + val + ' (' + time_diff + ' ms) </p>';
         result_div.append('<div class="nth_result">' + output + '</div>');
         output = '';
-    }
+        n++;
+    };
+    intervalID = setInterval(run_nth, 500);
+};
+
+var stopExecuting = function() {
+    if (intervalID)
+        clearInterval(intervalID);
 };
 
 window.onload = function () {
-    var go = $('#go-button')[0];
+    var go = $('#go')[0];
+    var stop = $('#stop')[0];
     go.onclick = execute;
+    stop.onclick = stopExecuting;
 };
 
 
