ef8cef9a17e05e1e307429198bd0eeb4bcce8bd5
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2015-11-25 11:43:57 -0500

    Second part, no hash table.

--- 20151125-unique-chars/unique-chars.js
+++ 20151125-unique-chars/unique-chars.js
@@ -29,3 +29,63 @@ var strings = [
 for (var i = 0; i < strings.length; i++) {
     console.log(strings[i] + ': ' + uniqueChars(strings[i]));
 }
+
+/**
+ * Implementation without a hash table.
+ * Algorithm:
+ * - take first char in string
+ * - iterate through string to look for that char
+ * - if not found, then this char was unique
+ * - keep list of chars already checked, don't check it again if it's there
+ *
+ */
+var uniqueCharsNoHash = function (s) {
+    var charBeingChecked;
+    var len = s.length;
+    for (var i = 0; i < len; i++) {
+        charBeingChecked = s[i];
+        for (var j = i + 1; j < len; j++) {
+            if (s[j] === charBeingChecked) return false;
+        }
+    }
+    return true;
+}
+
+/*
+abca    i   j   s[i]    s[j]    return
+^       0       a
+ ^          1           b
+  ^         2           c
+   ^        3           a       false
+
+
+abcd    i   j   s[i]    s[j]    return
+^       0       a
+ ^          1           b
+  ^         2           c
+   ^        3           d
+ ^      1       b
+            2           c
+            3           d
+  ^     2       c
+            3           d
+   ^    3       d
+            4           -
+                                true
+
+''      i   j   s[i]    s[j]    return
+^       0       ''
+                                true
+
+a       i   j   s[i]    s[j]    return
+^       0       q
+            1           -
+                                true
+ */
+
+console.log();
+console.log('no hash ...');
+for (var i = 0; i < strings.length; i++) {
+    console.log(strings[i] + ': ' + uniqueCharsNoHash(strings[i]));
+}
+
