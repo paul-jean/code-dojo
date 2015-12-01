c84883faed33ce1bedef08fac18c6808fd088d0f
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2015-12-01 13:38:19 -0500

    Flawed implementation

--- 20151201-delete-ll-node/delete-node.js
+++ 20151201-delete-ll-node/delete-node.js
@@ -1,29 +1,51 @@
 function LinkedList(dataArray) {
-    if (dataArray) {
+    if (dataArray.length >= 1) {
         this.data = dataArray[0];
         this.next = new LinkedList(dataArray.splice(1));
+        return this;
     } else {
-        this.data = null;
-        this.next = null;
+        return null;
     }
+}
+
+LinkedList.prototype.toArray = function() {
+    var current = this;
+    var nodeArray = [];
+    if (!current) return nodeArray;
+    else nodeArray.push(current.data);
+    while (current.next) {
+        nodeArray.push(current.next.data);
+        current = current.next;
+    }
+    return nodeArray;
+}
+
+LinkedList.prototype.hasNode = function(data) {
+    var current = this;
+    if (current.data === data) return true;
+    while (current.next) {
+        if (current.next.data === data) return true;
+        current = current.next;
+    }
+    return false;
 }
 
 LinkedList.prototype.deleteNode = function(data) {
     // {}
-    if (!this._head) return;
-    // {d}
+    if (!this) return;
+    // {c} -> {c.n} ...
     //  x
-    // {}
-    if (this._head.data === data) {
-        this._head = null;
+    // {c.n} ...
+    if (this.data === data) {
+        this = this.next;
         return;
     }
-    var current = this._head;
+    var current = this;
+    // {c} -> {c.n} -> {c.n.n} -> ...
+    //          x
+    // {c} ----------> {c.n.n} -> ...
     while (current.next) {
         if(current.next.data === data) {
-            // {c} -> {n} -> {n.n}
-            //         x
-            // {c} --------> {n.n}
             current.next = current.next.next;
             return;
         }
@@ -32,10 +54,16 @@ LinkedList.prototype.deleteNode = function(data) {
 }
 
 var t1 = new LinkedList([1, 2, 3]);
+console.log('t1');
+console.log(t1.toArray());
+console.log(t1.hasNode(2));
+/*
 t1.deleteNode(2);
 if (t1.hasNode(2))
     console.log('t1 failed');
+*/
 
+/*
 var t2 = new LinkedList([]);
 t2.deleteNode(2);
 if (t2.hasNode(2))
@@ -49,3 +77,4 @@ if (t3.hasNode(1))
 t3.deleteNode(2);
 if (!t3.hasNode(2))
     console.log('t4 failed');
+    */
