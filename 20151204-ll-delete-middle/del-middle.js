06638ba324ddd1c8e5e8234a75e6f1eef89a5f7c
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2015-12-04 22:39:31 -0500

    No idea how to delete an internal node from a ll given only access to that node, using a function call, in JS.

--- 20151204-ll-delete-middle/del-middle.js
+++ 20151204-ll-delete-middle/del-middle.js
@@ -1,7 +1,7 @@
 // how to force a call by reference to get a reference to the internal node?
-var deleteInternalNode = function (internalNode) {
-    if (!internalNode) return null;
-    internalNode[0] = internalNode[0].next;
+var deleteInternalNode = function (nodeObj) {
+    if (!nodeObj.node) return null;
+    nodeObj.node = nodeObj.node.next;
 }
 
 var ListNode = require('../lib/ListNode.js');
@@ -9,10 +9,18 @@ var ListNodeUtils = require('../lib/ListNode-utils.js');
 
 var t1 = new ListNode(0);
 t1 = ListNodeUtils.appendArray(t1, [1,2,3,4]);
+var nodeObj = {node: t1.next.next};
+
 console.log('test 1 ...');
-console.log(ListNodeUtils.toString(t1));
-var toDel = {0:t1.next.next};
-//deleteInternalNode(toDel);
+console.log(ListNodeUtils.toString(nodeObj.node));
+
+console.log('delete node {2}:');
+deleteInternalNode(nodeObj);
+
+console.log(ListNodeUtils.toString(nodeObj.node));
+
+/*
+console.log('Delete node {2} directly, without a function call:');
 t1.next.next = t1.next.next.next;
-console.log('delete node {2}');
 console.log(ListNodeUtils.toString(t1));
+*/
