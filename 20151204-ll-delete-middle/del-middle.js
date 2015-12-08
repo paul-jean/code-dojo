bf7c1283e915cded11b83b682970182fcf324fb0
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2015-12-07 23:10:23 -0500

    try to explain why fcn call doesnt work

--- 20151204-ll-delete-middle/del-middle.js
+++ 20151204-ll-delete-middle/del-middle.js
@@ -1,24 +1,31 @@
-// how to force a call by reference to get a reference to the internal node?
-var deleteInternalNode = function (nodeObj) {
-    if (!nodeObj.node) return null;
-    nodeObj.node = nodeObj.node.next;
-}
-
 var ListNode = require('../lib/ListNode.js');
 var ListNodeUtils = require('../lib/ListNode-utils.js');
 
-var t1 = new ListNode(0);
-t1 = ListNodeUtils.appendArray(t1, [1,2,3,4]);
-var nodeObj = {node: t1.next.next};
+var head = new ListNode(0);
+head = ListNodeUtils.appendArray(head, [1,2,3,4]);
 
 console.log('test 1 ...');
-console.log(ListNodeUtils.toString(t1));
+console.log(ListNodeUtils.toString(head));
 
 console.log('delete node {2} (with a function call):');
-deleteInternalNode(nodeObj);
+var node = head.next.next;
+// how to force a call by reference to get a reference to the internal node?
+var deleteInternalNode = function(node) {
+    if (!node || !node.next) return null;
+    node = node.next;
+}
+deleteInternalNode(node);
+console.log(ListNodeUtils.toString(head));
 
-console.log(ListNodeUtils.toString(nodeObj.node));
+console.log('delete node {2} (with a function taking a container obj):');
+var deleteInternalNodeFromObjRef = function(nodeObj) {
+    if (!nodeObj.node || !nodeObj.node.next) return null;
+    nodeObj.node = nodeObj.node.next;
+}
+var nodeObj = {node: head.next.next};
+deleteInternalNodeFromObjRef(nodeObj);
+console.log(ListNodeUtils.toString(head));
 
 console.log('Delete node {2} directly, without a function call:');
-t1.next.next = t1.next.next.next;
-console.log(ListNodeUtils.toString(t1));
+head.next.next = head.next.next.next;
+console.log(ListNodeUtils.toString(head));
