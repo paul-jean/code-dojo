79576cca3ca3999dfb65b9ed0784526cf93150f6
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2015-12-10 15:13:21 -0500

    Fix bugs. Add debug flag on prototype.

--- 20151210-stack-set/stack-set.js
+++ 20151210-stack-set/stack-set.js
@@ -1,3 +1,5 @@
+var process = require("process");
+
 function StackSet(threshold) {
     this.headArray = [];
     this.len = 0;
@@ -9,15 +11,32 @@ function StackNode(data) {
     this.next = null;
 }
 
+StackSet.prototype.debug = true;
+
 StackSet.prototype.push = function(data) {
-    var n = new StackNode(data); // TODO: implement
-    if (this.len === this.threshold || !this.headArray) {
+    var n = new StackNode(data);
+    if (this.len === this.threshold || this.headArray.length === 0) {
+        console.log("(StackSet) push: " + data + " (into NEW stack)");
         this.headArray.push(n);
         this.len = 1;
     } else {
+        console.log("(StackSet) push: " + data);
         var head = this.headArray[this.headArray.length - 1];
         n.next = head;
+        this.headArray[this.headArray.length - 1] = n;
         this.len = this.len + 1;
+    }
+    if (this.debug) {
+        console.log("(StackSet) push: headArray = ");
+        for (var h of this.headArray) {
+            var c = h;
+            while (c) {
+                process.stdout.write(c.data + " - ");
+                c = c.next;
+            }
+            process.stdout.write("||");
+            console.log();
+        }
     }
 }
 
@@ -28,5 +47,22 @@ StackSet.prototype.pop = function() {
     head = head.next;
     if (!head) {
         this.headArray.slice(0, this.headArray.length - 1);
+        if (this.debug)
+            console.log("(StackSet) pop: num internal stacks = " + this.headArray.length);
+    } else {
+        this.headArray[this.headArray.length - 1] = head;
     }
+    return ret;
 }
+
+var ss = new StackSet(3);
+ss.push(1);
+ss.push(2);
+ss.push(3);
+console.log("(len = " + ss.len + ")"); // (len = 3)
+ss.push(4);
+console.log("(len = " + ss.len + ")"); // (len = 1)
+console.log(ss.pop()); // 4
+console.log(ss.pop()); // 3
+ss.push(3);
+console.log(ss.pop()); // 3
