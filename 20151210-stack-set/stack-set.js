053c3367312988d8a3da9cba4b3b246c2f03a7d4
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2015-12-10 17:13:02 -0500

    Fix counter measuring most recent stack size

--- 20151210-stack-set/stack-set.js
+++ 20151210-stack-set/stack-set.js
@@ -31,49 +31,69 @@ StackSet.prototype.pop = function() {
     head = head.next;
     if (!head) {
         this.headArray = this.headArray.slice(0, this.headArray.length - 1);
+        if (this.headArray.length > 0) {
+            this.len = this.threshold;
+        } else {
+            this.len = 0;
+        }
     } else {
         this.headArray[this.headArray.length - 1] = head;
+        this.len = this.len - 1;
     }
     return ret;
 };
 
 StackSet.prototype.toString = function() {
+    if (this.headArray.length === 0) return "(empty)\n";
     var s = "";
+    var i = 0;
     for (var h of this.headArray) {
+        s += "stack " + i + ": ";
         var c = h;
         while (c) {
             s += c.data + " - ";
             c = c.next;
         }
         s += "||\n";
+        i++;
     }
     return s;
 };
 
+StackSet.prototype.stateString = function() {
+    var s = "";
+    s += "++++++++\n";
+    s += this.toString();
+    s += "len = " + this.len;
+    s += "\n++++++++";
+    return s;
+};
+
 var ss = new StackSet(3);
-console.log("push 1:");
 ss.push(1);
-console.log(ss.toString());
-console.log("push 2:");
+console.log("push 1:");
+console.log(ss.stateString());
 ss.push(2);
-console.log(ss.toString());
-console.log("push 3:");
+console.log("push 2:");
+console.log(ss.stateString());
 ss.push(3);
-console.log(ss.toString());
-console.log("(len = " + ss.len + ")"); // (len = 3)
-console.log("push 4:");
+console.log("push 3:");
+console.log(ss.stateString());
 ss.push(4);
-console.log(ss.toString());
-console.log("(len = " + ss.len + ")"); // (len = 1)
+console.log("push 4:");
+console.log(ss.stateString());
 console.log("pop: " + ss.pop()); // 4
-console.log(ss.toString());
+console.log(ss.stateString());
 console.log("pop: " + ss.pop()); // 3
-console.log(ss.toString());
-console.log("push 3:");
+console.log(ss.stateString());
 ss.push(3);
-console.log(ss.toString());
+console.log("push 3:");
+console.log(ss.stateString());
 console.log("pop: " + ss.pop()); // 3
-console.log(ss.toString());
+console.log(ss.stateString());
 console.log("pop: " + ss.pop()); // 2
+console.log(ss.stateString());
 console.log("pop: " + ss.pop()); // 1
+console.log(ss.stateString());
 console.log("pop: " + ss.pop()); // null
+console.log(ss.stateString());
